b68716b3f7cf4806e3cb9f674e975815237bdb90
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2024-04-15 10:27:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,4 +65,6 @@ def scrape():
     return jsonify(scraped_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Usando a porta fornecida pelo Heroku ou, se não estiver disponível, a 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
