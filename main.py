116d7b8648a8785be88e9048df6dddefb7cf218c
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2024-04-17 19:53:34 +0100

    backend

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
+CORS(app)  # Permite solicitações de qualquer origem
 
 # Rota principal
-@app.route('/')
-def index():
+@app.route('/api/pergunta')
+def get_question():
     # Obter o número da questão da query string, ou 0 se não estiver presente
     question_number = int(request.args.get('numero', 1))
 
@@ -47,7 +49,7 @@ def index():
                 # Adicionar o texto da resposta à lista de textos das respostas
                 answer_texts.append(answer_text)
             # Encontrar a resposta correta, se existir
-            correct_answer_element = answers_element.find("li", class_="A correct")
+            correct_answer_element = soup.find(class_="answer correct")
             correct_answer = correct_answer_element.get_text(strip=True) if correct_answer_element else "Nenhuma resposta correta encontrada."
         else:
             # Se nenhum elemento com a classe 'answers' for encontrado, definir uma mensagem de resposta padrão
@@ -57,9 +59,11 @@ def index():
         # URL da imagem
         image_url = f"https://www.bomcondutor.pt/assets/images/questions/{question_number}.jpg"
 
+        # Renderizar o template HTML com os dados da pergunta
         return render_template('template.html', question_text=question_text, answer_texts=answer_texts, correct_answer=correct_answer, image_url=image_url, question_number=question_number)
     else:
-        return "Erro ao acessar a página:", response.status_code
+        # Se ocorrer um erro ao acessar a página, retornar um erro HTTP 500
+        return jsonify({"error": "Erro ao acessar a página"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
